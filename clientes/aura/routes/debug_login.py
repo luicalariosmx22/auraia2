d6576fe257cac1668f5970250ce0d3ffded17753
luicalariosmx22 <hola@luicalarios.com>
2025-04-11 16:53:06 -0700

    mal puestos

---
+++ clientes/aura/routes/debug_login.py
@@ -0,0 +1,19 @@
+# clientes/aura/routes/debug_login.py
+from flask import Blueprint, session, jsonify
+
+debug_login_bp = Blueprint("debug_login", __name__)
+
+@debug_login_bp.route("/debug/login_info")
+def login_info():
+    if "user" in session:
+        return jsonify({
+            "logged_in": True,
+            "name": session["user"].get("name"),
+            "email": session["user"].get("email"),
+            "is_admin": session.get("is_admin", False)
+        })
+    else:
+        return jsonify({
+            "logged_in": False,
+            "message": "Ningún usuario está logueado actualmente."
+        })
