5cd165885d7bf5eb42567ebb8be8d6f3955db49b
luicalariosmx22 <hola@luicalarios.com>
2025-04-11 13:14:17 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,15 @@ from clientes.aura.routes.chat_data import chat_data_bp
 
 app = Flask(__name__, static_url_path="/static", static_folder="clientes/aura/static")
 
-# Registrar rutas de Aura
+# Registrar rutas del bot Aura
 app.register_blueprint(webhook_bp)
 app.register_blueprint(panel_chat_bp)
 app.register_blueprint(chat_data_bp)
 
-# Ruta raíz opcional
+# Ruta raíz
 @app.route("/")
 def home():
-    return "Servidor activo"
+    return "Servidor activo y corriendo."
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
