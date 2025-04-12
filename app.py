ffe8e8768fde146647838d5d9befac67703aedd8
luicalariosmx22 <hola@luicalarios.com>
2025-04-12 05:00:56 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
+# ========= CONFIGURACIÓN FLASK =========
+app = Flask(
+    __name__,
+    template_folder='clientes/aura/templates',
+    static_folder='clientes/aura/static'
+)
+
+app.session_cookie_name = app.config.get("SESSION_COOKIE_NAME", "session")
+app.secret_key = os.getenv("SECRET_KEY", "clave-secreta-por-defecto")
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# ========= REGISTRO DE BLUEPRINTS DESPUÉS DE DEFINIR "app" =========
+from clientes.aura.registro.registro_login import registrar_blueprints_login
 from clientes.aura.registro.registro_base import registrar_blueprints_base
 from clientes.aura.registro.registro_cliente import registrar_blueprints_cliente
 from clientes.aura.registro.registro_admin import registrar_blueprints_admin
 from clientes.aura.registro.registro_debug import registrar_blueprints_debug
+from clientes.aura.registro.registro_dinamico import registrar_blueprints_por_nora
 
+registrar_blueprints_login(app)
 registrar_blueprints_base(app)
 registrar_blueprints_cliente(app)
 registrar_blueprints_admin(app)
 registrar_blueprints_debug(app)
+registrar_blueprints_por_nora(app, "aura")
