e97336b83dbd23b6e044bb87199c6fafd41352b7
luicalariosmx22 <hola@luicalarios.com>
2025-04-18 02:07:42 -0700

    Update app.py

--- app.py
+++ app.py
@@ -27,6 +27,7 @@ from clientes.aura.registro.registro_admin import registrar_blueprints_admin
 from clientes.aura.registro.registro_debug import registrar_blueprints_debug
 from clientes.aura.registro.registro_dinamico import registrar_blueprints_por_nora
 from clientes.aura.routes.panel_chat import panel_chat_bp
+from clientes.aura.routes.webhook import webhook_bp  # Importar el blueprint del webhook
 
 registrar_blueprints_login(app)
 registrar_blueprints_base(app)
@@ -35,6 +36,7 @@ registrar_blueprints_admin(app)
 registrar_blueprints_debug(app)
 registrar_blueprints_por_nora(app, "aura")
 app.register_blueprint(panel_chat_bp)
+app.register_blueprint(webhook_bp)  # Registrar el blueprint del webhook
 
 # ========= RUTA INICIAL =========
 @app.route("/")
