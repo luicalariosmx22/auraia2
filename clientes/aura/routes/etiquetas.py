4ffc7f3fd3f7b3b10f59aa00baf68f0db7e57b9a
luicalariosmx22 <hola@luicalarios.com>
2025-04-19 00:48:42 -0700

    Update etiquetas.py

--- clientes/aura/routes/etiquetas.py
+++ clientes/aura/routes/etiquetas.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session, flash
 from supabase import create_client
 from dotenv import load_dotenv
-from clientes.utils.config import login_requerido
+from clientes.aura.utils.config import login_requerido
 import os
 
 # Configurar Supabase
