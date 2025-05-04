50a97752750eedc456a489e4e7868439cf378117
luicalariosmx22 <hola@luicalarios.com>
2025-05-04 01:02:31 -0700

    borrar

--- clientes/aura/routes/panel_chat.py
+++
@@ -1,7 +0,0 @@
-print("✅ panel_chat.py cargado correctamente")
-
-from flask import Blueprint
-
-panel_chat_bp = Blueprint("panel_chat", __name__)
-
-from clientes.aura.routes.panel_chat_routes import *  # Importa SOLO las rutas limpias
