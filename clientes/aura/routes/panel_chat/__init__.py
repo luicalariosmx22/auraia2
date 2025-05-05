0a048c67f1839a9be69f4af6e7702a605259cddf
luicalariosmx <156865059+luicalariosmx@users.noreply.github.com>
2025-05-04 23:26:51 -0700

    Update __init__.py

--- clientes/aura/routes/panel_chat/__init__.py
+++ clientes/aura/routes/panel_chat/__init__.py
@@ -6,6 +6,6 @@ from clientes.aura.routes.panel_chat.blueprint import panel_chat_bp
 from clientes.aura.routes.panel_chat import vista_panel_chat
 from clientes.aura.routes.panel_chat import vista_api_chat
 from clientes.aura.routes.panel_chat import vista_enviar_mensaje
+from clientes.aura.routes.panel_chat import vista_gestion_etiqueta
 from clientes.aura.routes.panel_chat import vista_toggle_ia
 from clientes.aura.routes.panel_chat import vista_programar_envio
-from clientes.aura.routes.panel_chat import vista_gestion_etiqueta
