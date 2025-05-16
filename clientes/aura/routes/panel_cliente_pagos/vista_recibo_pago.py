39581635894cb4d73835a9094656cabc4153496e
luicalariosmx22 <hola@luicalarios.com>
2025-05-15 18:24:18 -0700

    Update vista_recibo_pago.py

--- clientes/aura/routes/panel_cliente_pagos/vista_recibo_pago.py
+++ clientes/aura/routes/panel_cliente_pagos/vista_recibo_pago.py
@@ -5,7 +5,7 @@ from supabase import create_client
 import os, shutil, subprocess, tempfile
 from pathlib import Path
 from clientes.aura.utils.login_required import login_required
-from fpdf2 import FPDF
+from fpdf import FPDF
 from reportlab.pdfgen import canvas
 from PyPDF2 import PdfFileWriter
 
