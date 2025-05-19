8866234740caebf02a6645a8c618969278ae5ae4
luicalariosmx22 <hola@luicalarios.com>
2025-05-19 16:53:31 -0700

    Update registro_login.py

--- clientes/aura/registro/registro_login.py
+++ clientes/aura/registro/registro_login.py
@@ -3,4 +3,5 @@ from flask import Blueprint, request, redirect, url_for, render_template
 from clientes.aura.auth.login import login_bp
 
 def registrar_blueprints_login(app, safe_register_blueprint):
-    safe_register_blueprint(app, login_bp, url_prefix="")
+    # ✅ Corregido para que  funcione correctamente
+    safe_register_blueprint(app, login_bp, url_prefix="/login")
