0c63f0c5a876ad7b9e9f9bdb750320d4a6c59c0d
luicalariosmx22 <hola@luicalarios.com>
2025-05-19 22:58:59 -0700

    Update login_required.py

--- clientes/aura/utils/login_required.py
+++ clientes/aura/utils/login_required.py
@@ -8,3 +8,11 @@ def login_required(f):
             return redirect("/login")
         return f(*args, **kwargs)
     return decorated_function
+
+def login_required_cliente(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not session.get("email") or not session.get("nombre_nora"):
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
