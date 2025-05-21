ac28ca86b3db2c3a65e59142ac8c216aa8db7d37
luicalariosmx22 <hola@luicalarios.com>
2025-05-21 01:35:30 -0700

    Update __init__.py

--- clientes/aura/routes/panel_cliente_tareas/__init__.py
+++ clientes/aura/routes/panel_cliente_tareas/__init__.py
@@ -5,8 +5,12 @@ from flask import Blueprint
 panel_cliente_tareas_bp = Blueprint(
     "panel_cliente_tareas",
     __name__,
-    template_folder="../../templates/panel_cliente_tareas"
+    template_folder="../../../templates/panel_cliente_tareas"
 )
+
+@panel_cliente_tareas_bp.route("/")
+def tareas_home():
+    return "✅ Módulo de tareas activo"
 
 # Importa los submódulos que registran rutas en este blueprint
 from . import (
